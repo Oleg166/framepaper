fa10b8dfb083564b2ec1d12d593c0a3db25eca83
Dementev Oleg <demeoleg@mail.ru>
2021-02-08 19:23:29 +0300

    Change text

--- framepaper/core.py
+++ framepaper/core.py
@@ -6,4 +6,4 @@ def application(environ, start_response):
     # сначала в функцию start_response передаем код ответа и заголовки
     start_response('200 OK', [('Content-Type', 'text/html')])
     # возвращаем тело ответа в виде списка из bite
-    return [b'Hello world from a simple WSGI application!']
+    return [b'Hello WORLD from a simple WSGI application!']
